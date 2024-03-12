3536a765e1dd77946cd6423a8c9dc01fb5e9fe3d
Mehul <mehtomar@gmail.com>
2024-03-12 00:14:52 -0400

    Reads all messages and checks for the command symbol

--- bot.js
+++ bot.js
@@ -1,11 +1,26 @@
 import 'dotenv/config'
 import {Client, Events, GatewayIntentBits} from 'discord.js'
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]});
+const commandSymbol = '!';
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Logged in as ${readyClient.user.tag}`);
 });
+
+client.on(Events.MessageCreate, async (msg) => {
+    checkCommands(msg);
+});
+
+function checkCommands(msg) {
+    if(!msg.author.bot && msg.content.startsWith(commandSymbol)) {
+        msg.reply('This is a command!');
+    }
+}
 
 client.login(process.env.Token);
 
