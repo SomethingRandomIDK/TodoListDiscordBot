bd3144df301298e058f16b9d5a00ff982a184e3f
Mehul <mehtomar@gmail.com>
2024-03-12 00:59:02 -0400

    Added basic commands

--- bot.js
+++ bot.js
@@ -6,7 +6,25 @@ const client = new Client({intents: [
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
 ]});
+const tasks = []
 const commandSymbol = '!';
+const commands = {
+    addTask: async function(msg) {
+        const args = msg.content.split(' ');
+        args.shift();
+        let task = '';
+        for (let x of args) {
+            task += x + ' ';
+            console.log(x);
+        }
+        tasks.push(task)
+    },
+    listTasks: async function (msg) {
+        for (let x of tasks) {
+            msg.channel.send(x);
+        }
+    }
+};
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Logged in as ${readyClient.user.tag}`);
@@ -18,7 +36,7 @@ client.on(Events.MessageCreate, async (msg) => {
 
 function checkCommands(msg) {
     if(!msg.author.bot && msg.content.startsWith(commandSymbol)) {
-        msg.reply('This is a command!');
+        if(commands[msg.content.split(' ')[0].slice(1)](msg));
     }
 }
 
